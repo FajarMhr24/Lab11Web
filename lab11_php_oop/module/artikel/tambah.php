4f675a8b1b2a46d3e11c06ab74d7e44906b0aaa4
Shironeko <fajarmaher256@gmail.com>
2025-12-14 18:11:15 +0700

    Add form to create new articles in tambah.php

---
+++ lab11_php_oop/module/artikel/tambah.php
@@ -0,0 +1,23 @@
+<?php
+$form = new Form("", "Simpan Artikel");
+
+if ($_POST) {
+    $db = new Database();
+    $db->insert('artikel', [
+        'judul' => $_POST['judul'],
+        'isi'   => $_POST['isi']
+    ]);
+
+    header("Location: /lab11_php_oop/index.php/artikel/index");
+    exit;
+}
+?>
+
+<h3>Tambah Artikel</h3>
+
+<?php
+$form->addField("judul", "Judul Artikel");
+$form->addField("isi", "Isi Artikel", "textarea");
+
+$form->displayForm();
+?>
