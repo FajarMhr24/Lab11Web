9f3d271dc9d15576621dca3faea8f2783adbb698
Shironeko <fajarmaher256@gmail.com>
2025-12-14 18:08:47 +0700

    Add header.php for Modular Framework Lab 11

---
+++ lab11_php_oop/template/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Modular Framework Lab 11</title>
+    <link rel="stylesheet" href="/lab11_php_oop/assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+<header>
+    <h2>Modular Framework Lab 11</h2>
+    <nav>
+        <a href="/lab11_php_oop/">Home</a> |
+        <a href="/lab11_php_oop/artikel/index">Artikel</a>
+    </nav>
+</header>
+<hr>
